8a9bd17b835b48227e45946ae528e4f7b892b0f9
eeeXun <sdes96303@gmail.com>
2022-12-05 00:08:57 +0800

    rename output to output_file

--- StegoKit.py
+++ StegoKit.py
@@ -5,18 +5,18 @@ class StegoKit:
     def __init__(self, file: str):
         self.file: str = file
 
-    def hide(self, input: str, output: str, input_type: str = "string"):
+    def hide(self, input: str, output_file: str, input_type: str = "string"):
         """
         input_type should be "string" or "file", default is "string"
 
         If input_type is "string", input should be string
         If input_type is "file"", input should be file name
 
-        output should be file name to save after hide data
+        output_file should be file name to save after hiding data
 
         ex.
-        StegoKit("a.png").hide(input="ABCD", output="b.png", input_type="string")
-        StegoKit("a.png").hide(input="a.txt", output="b.png", input_type="file")
+        StegoKit("a.png").hide(input="ABCD", output_file="b.png", input_type="string")
+        StegoKit("a.png").hide(input="a.txt", output_file="b.png", input_type="file")
         """
         input_data: bytes
         mime_type = magic.from_file(self.file, mime=True).split("/")
