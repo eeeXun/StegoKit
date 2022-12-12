c62713b21c86956d62a057d9d0983663a89c5b23
eeeXun <sdes96303@gmail.com>
2022-12-12 21:42:43 +0800

    add some comment

--- Audio.py
+++ Audio.py
@@ -6,5 +6,11 @@ class StegoAudio:
         self.data = AudioSegment.from_file(file, format=format)
 
     def hide(self, input_data: bytes, output_file: str):
+        """
+        Hide data with LSB steganography.
+        Hide data length in first 16 bits of samples. So maximum of data would be 65536 bit.
+        """
         index = 0
         samples = self.data.get_array_of_samples()
+
+        # hide data length in first 16 bits of samples
