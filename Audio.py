6306904c3b9e932b9fc11fd143c131ef4db697da
eeeXun <sdes96303@gmail.com>
2022-12-12 23:53:57 +0800

    feat: hide in audio

--- Audio.py
+++ Audio.py
@@ -3,14 +3,29 @@ from pydub import AudioSegment
 
 class StegoAudio:
     def __init__(self, file: str, format: str):
-        self.data = AudioSegment.from_file(file, format=format)
+        self.samples = AudioSegment.from_file(file, format=format).get_array_of_samples()
 
-    def hide(self, input_data: bytes, output_file: str):
+    def hide(self, input_data: list[str], output_file: str):
         """
         Hide data with LSB steganography.
-        Hide data length in first 16 bits of samples. So maximum of data would be 65536 bit.
+        Hide data length in first 16 samples. So maximum of data would be 65536 bit.
         """
         index = 0
-        samples = self.data.get_array_of_samples()
 
         # hide data length in first 16 bits of samples
+        data_len = len(input_data) * 8
+        if len(self.samples) < data_len + 16:
+            raise Exception("The amount of samples is too small to hide data")
+        elif data_len > 65536:
+            raise Exception("The data exceed the maximum number(65536) you can hide")
+        else:
+            # hide length
+            data_len_bin = bin(data_len)[2:].rjust(16, "0")
+            for c in data_len_bin:
+                self.samples[index] = (self.samples & 0xFFFFFFFFFFFFFFFE) | int(c)
+                index += 1
+            # hide data
+            for bin_str in input_data:
+                for c in bin_str:
+                    self.samples[index] = (self.samples & 0xFFFFFFFFFFFFFFFE) | int(c)
+                    index += 1
